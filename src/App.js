e28aed7ff7bc7b5fc66a2988d633f0c44636d05c
Edward Wenger <wenger.edward@gmail.com>
2016-09-18 23:55:58 -0700

    add team-pick onClick

--- src/App.js
+++ src/App.js
@@ -16,39 +16,76 @@ var teams = ['det', 'ten', 'hou', 'kc', 'ne', 'mia'];
 var team_logos = {};
 teams.forEach((name) => {
     team_logos[name] = require("./teams/" + name + ".png")
-})
-
-var Game = React.createClass({
-    displayName: 'Game',
-    render: function () {
-        return (
-            <tr className="Game-tr">
-                <td className="Game-td-helmet"><img src={team_logos[this.props.favorite]} alt=""/></td>
-                <td className="Game-td-left">{this.props.favorite} -{this.props.spread}</td>
-                <td className="Game-td-right">{this.props.underdog}</td>
-                <td className="Game-td-helmet"><img src={team_logos[this.props.underdog]} alt=""/></td>
-            </tr>
-        );
-    }
 });
 
-var GameList = React.createClass({
-    displayName: 'GameList',
-    render: function () {
+class GameTeamHelmet extends Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.handleTeamPick(this.props.team)
+    }
+
+    render() {
+        return (
+            <div className="GameTeamHelmet-td-helmet" onClick={this.handleClick}>
+                <img src={team_logos[this.props.team]} alt=""/>
+            </div>
+        );
+    }
+}
+
+class GameTeam extends Component {
+    render() {
+        var textStyle = this.props.picked ? {fontWeight: "bold"} : {};
+        return (
+            <div className="GameTeam" style={textStyle}>{this.props.team}{this.props.spread}</div>
+        );
+    }
+}
+
+class Game extends Component {
+    constructor() {
+        super();
+        this.state = {pick: null};
+        this.pickTeam = this.pickTeam.bind(this);
+    }
+
+    pickTeam(team) {
+        this.setState({pick: team});
+    }
+
+    render() {
+        const spread = ' -' + this.props.spread;
+        const pickedFavorite = (this.props.favorite === this.state.pick);
+        const pickedUnderdog = (this.props.underdog === this.state.pick);
+        return (
+            <div className="Game">
+                <GameTeamHelmet team={this.props.favorite} handleTeamPick={this.pickTeam}/>
+                <GameTeam team={this.props.favorite} spread={spread} picked={pickedFavorite}/>
+                <GameTeam team={this.props.underdog} spread='' picked={pickedUnderdog}/>
+                <GameTeamHelmet team={this.props.underdog} handleTeamPick={this.pickTeam}/>
+            </div>
+        );
+    }
+}
+
+class GameList extends Component {
+    render() {
         var gameNodes = this.props.data.map(function (game) {
             return (
                 <Game key={game.id} favorite={game.favorite} underdog={game.underdog} spread={game.spread}/>
             );
         });
         return (
-            <table className="GameList-table">
-                <tbody>
+            <div className="GameList">
                 {gameNodes}
-                </tbody>
-            </table>
+            </div>
         );
     }
-});
+}
 
 class App extends Component {
     render() {
